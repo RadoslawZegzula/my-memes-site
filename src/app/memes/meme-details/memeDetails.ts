a76f4b432ef35b8caa17a6ba66f255302b19349a
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-06 17:36:54 +0200

    Add memeDetailsComponent

---
+++ src/app/memes/meme-details/memeDetails.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meme-details',
+  template: 
+  `
+  <div>
+    meme details
+  </div>
+  `,
+  styles: [``]
+})
+
+export class MemeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

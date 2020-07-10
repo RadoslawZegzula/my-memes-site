91e6e72c13cb085b25457a96eed545abf5117c38
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-10 21:59:59 +0200

    Add meme and toastr service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AppRoutes} from './routes'
+import {AppRoutes} from './routes';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -9,19 +9,25 @@ import { MemesComponent } from './memes/memes.component';
 import { MemeMemberOfList } from './memes/member-of-memes-list/memeMemberOfList';
 import { MemeDetailsComponent } from './memes/meme-details/memeDetails';
 
-@NgModule({
-  declarations: [
+import { MemeService } from './memes/shared/meme.service';
+import { ToastrService } from './common/toastrService'
+
+@NgModule(
+  {
+  declarations: 
+  [
     AppComponent,
     NavbarComponent,
     MemesComponent,
     MemeMemberOfList,
     MemeDetailsComponent
   ],
-  imports: [
+  imports: 
+  [
     BrowserModule,
     RouterModule.forRoot(AppRoutes)
   ],
-  providers: [],
+  providers: [MemeService, ToastrService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

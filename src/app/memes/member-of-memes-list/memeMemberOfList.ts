e14e2efc91f1c0773eaf036c1b2c5dbd93866b67
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-11 19:33:59 +0200

    change styling

--- src/app/memes/member-of-memes-list/memeMemberOfList.ts
+++ src/app/memes/member-of-memes-list/memeMemberOfList.ts
@@ -5,18 +5,25 @@ import { ToastrService } from 'src/app/common/toastrService';
   selector: 'meme-member-of-list',
   template: 
   `
-  <div>
+  <div class="well hoverwell thumbnail">
     <h3 [routerLink]="['/memes', meme.id]">: {{meme.title}}</h3>
-    <img [routerLink]="['/memes', meme.id]" src="./assets/img/img1.jpg"/>
+    <img [routerLink]="['/memes', meme.id]" src="./assets/img/img2.jpg"/>
     <div>
-        <button (click)="handleClickedLike()">Like</button>
-        <button (click)="handleClickedShare()">Share</button>
-        <button (click)="handleClickedDownload()">Download</button>
+        <button class="btn btn-default" (click)="handleClickedLike()">
+        Likes: <span class="badge">9</span>
+        </button>
+        <button class="btn btn-default" (click)="handleClickedShare()">Share</button>
+        <button class="btn btn-default" (click)="handleClickedDownload()">Download</button>
     </div>
-    <button>Show comments</button>
+    <button class="btn btn-default">
+    Show comments <span class="badge">67</span>
+    </button>
   </div>
   `,
-  styles: [``]
+  styles: 
+  [`
+    .thumbnail { min-height: 250px;margin-left:30%;margin-right: 30%;}
+  `]
 })
 
 export class MemeMemberOfList implements OnInit {

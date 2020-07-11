5067342ad3513ca0c0e612ea80b9c3f0695f0ed9
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-11 15:41:24 +0200

    Add more memes to array

--- src/app/memes/shared/meme.service.ts
+++ src/app/memes/shared/meme.service.ts
@@ -35,4 +35,52 @@ const arrayOfMemes =
       title : "th",
       urlOfImage : "WWW"
     },
+    {
+      id : 4,
+      name : "MemeNr1",
+      title : "First",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 5,
+      name : "MemeNr2",
+      title : "Second",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 6,
+      name : "MemeNr3",
+      title : "th",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 7,
+      name : "MemeNr1",
+      title : "First",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 8,
+      name : "MemeNr2",
+      title : "Second",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 9,
+      name : "MemeNr3",
+      title : "th",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 10,
+      name : "MemeNr1",
+      title : "First",
+      urlOfImage : "WWW"
+    },
+    {
+      id : 11,
+      name : "MemeNr2",
+      title : "Second",
+      urlOfImage : "WWW"
+    }
   ]

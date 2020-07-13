05332d4ca706cd5f2a3f6b19832fb684ef7a701f
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-13 16:10:04 +0200

    Add 404 component

---
+++ src/app/errors/error404component.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core'
+
+@Component({
+  selector: 'error-message',
+  template: 
+  `
+  <h2>
+    This is id 404 error Messagee
+  </h2>
+  `,
+  styles: [``]
+})
+
+export class Error404Component
+{
+}

b978fe9235eba943c38878a5e768e92c0786a027
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-21 20:39:19 +0200

    Implement login form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
+
 import { UserRoutes } from './user.routes'
 import { UserProfileComponent } from './user-profile.component'
+import { LoginComponent } from './login.component'
 
 @NgModule(
     {
     declarations: 
     [
-        UserProfileComponent
-
+        UserProfileComponent,
+        LoginComponent
     ],
     imports: 
     [
         CommonModule,
-        RouterModule.forChild(UserRoutes)
-
+        RouterModule.forChild(UserRoutes),
+        FormsModule
     ],
     providers: 
     [
-
     ],
   })
   export class UserModule { }
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,8 @@
 import { UserProfileComponent } from './user-profile.component'
+import { LoginComponent } from './login.component'
 
 export const UserRoutes = 
 [
-    {path: 'profile', component: UserProfileComponent}
+    { path: 'profile', component: UserProfileComponent },
+    { path: 'login', component: LoginComponent }
 ]

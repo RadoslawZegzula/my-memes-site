287fc2b19d8f3107d1ef457d520d0dd7b2f59051
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-25 11:26:45 +0200

    Add auth service

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core'
+import { IUser } from './user.model'
+
+@Injectable()
+export class AuthService
+{
+    currentUser:IUser
+
+    loginUser(userName:string, password:string)
+    {
+        this.currentUser = 
+        {
+            id:1,
+            firstName:"john",
+            lastName:"Paulo",
+            userName:userName
+        }
+    }
+
+    isAuthenticated()
+    {
+        return !!this.currentUser;
+    }
+
+}

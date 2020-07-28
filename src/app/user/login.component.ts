2036b5fa864788ff6bb1b780cf3e6cb5ff655240
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-28 21:26:25 +0200

    Implement validation in login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,13 +4,23 @@ import { AuthService } from './auth.service'
 
 @Component
 ({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: 
+  [`
+  em
+  {
+    float:right; 
+    color:#E05C65; 
+    padding-left:10px;
+  }
+  `]
 })
 export class LoginComponent
 {
   userName
   password
-  
+  mouseoverLogin
+
   constructor(private authService:AuthService, private router:Router)
   {
     

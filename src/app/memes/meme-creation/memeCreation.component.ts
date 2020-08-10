c718b2ac092d00125e7b1658be82fd8120f4157b
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-10 11:24:32 +0200

    Update meme creation component

--- src/app/memes/meme-creation/memeCreation.component.ts
+++ src/app/memes/meme-creation/memeCreation.component.ts
@@ -1,39 +1,35 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
 import { MemeService } from '../shared/meme.service'
+import { IMeme } from '../shared/meme.model';
 
 @Component
 ({
   selector: 'meme-details',
-  template: 
-  `
-  <div>
-    <h2>Here you can add meme</h2>
-    Name:
-    <div>
-      <button class="btn btn-primary" (click)="addMeme()">Add</button>
-      <button class="btn btn-default" (click)="cancel()">Cancel</button>
-    </div>
-  </div>
-  `,
-  styles: [``]
+  templateUrl: './memeCreation.component.html',
+  styles: 
+  [`
+  em { float:right, color:#E05C65; padding-left:10px; }
+  .error input { background-color:#E3C3C3; }
+  .error ::-webkit-input-placeholder { color:999; }
+  .error ::-moz-placeholder { color:999; }
+  .error :-moz-placeholder { color:999; }
+  .error :ms-input-placeholder { color:999; }
+  `]
 })
 
 export class MemeCreationComponent
 {
-  isDirty:boolean = true;
+  newMeme:IMeme
+  isDirty:boolean = true
 
   constructor(private memeService:MemeService, private router:Router) { }
 
-  addMeme()
+  saveMeme(formValues:IMeme)
   {
-    this.memeService.addMeme
-    ({
-      id : 155,
-      name : "Created meme",
-      title : "CreatedMeme",
-      urlOfImage : "WWW"
-    });
+    this.memeService.addMeme(formValues)
+    this.isDirty = false
+    this.router.navigate(['/memes'])
   }
 
   cancel()
